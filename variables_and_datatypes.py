ba4779100d2c735e0d1362c466bb72e85cffa3c2
emwadun <duncan.owino@gmail.com>
2022-02-07 13:03:19 +0000

    first commit

---
+++ variables_and_datatypes.py
@@ -0,0 +1,7 @@
+his_name = "Owino"
+his_age = 39
+is_male = True
+
+print("There was once a man named " +his_name+ ",")
+print("He was ",his_age)
+print("was he a male", is_male)
---
+++ working_with_string.py
@@ -0,0 +1,29 @@
+phrase = "Giraffe Academy"
+print(phrase)
+
+print(phrase+ "is cool")
+
+print ("Giraffe Academy")
+
+print("Giraffe \nAcademy")
+
+print(phrase.lower())
+print(phrase.upper())
+
+#phrase = phrase.lower()
+
+print(phrase.islower())
+
+#phrase = phrase.upper()
+print(phrase.isupper())
+print(phrase.upper().isupper())
+
+
+print(len(phrase))
+
+print(phrase[14])
+
+
+#print(phrase.index("W"))
+
+print(phrase.replace("Giraffe", "Lion"))
