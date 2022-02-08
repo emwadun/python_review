59804936f55fdcb1745d70c504be5697d98b07f6
emwadun <duncan.owino@gmail.com>
2022-02-08 16:46:22 +0000

    updates

---
+++ working_with_numbers.py
@@ -0,0 +1,32 @@
+print(2)
+
+print(3.134)
+
+print(3*4)
+
+print(5%2)
+
+print(5/2)
+
+print(6 + 2 * 4)
+
+x = 40
+
+print(x)
+
+print(str(x)+ " is my age")
+
+
+y = -23
+print(abs(y))
+
+x=2
+y=3
+
+print(pow(x,y))
+
+print(max(x,y))
+
+print(min(x,y))
+
+print(round(3.7))
